ceea8d24a386b90d185ec4408f53f4088a19f824
Rafael Souza <rsouza19796@gmail.com>
2020-01-08 11:36:56 -0300

    autotest: Added autotesting features.

---
+++ build/utilities/autotest/test.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+source header.sh
+source .env
+
+# Automatic testing script for AVAPolos solution.
+
+# Ask if the machines need to be imaged.
+# If yes, reboot and image them,
+# Use FOG API to get MAC Adresses to WOL them back up, use fog api to check for the task status.
+# Send installer via scp, and then install it.
+
+# Future versions:
+# After installing, add data to moodle and clone it.
+# Send to the POLO machine and install it.
+
+# Stellar version:
+# Fully automatic testing.
+
+
+greet
+
+test() {
+  #shutdown_host $IP_IES wait
+  #shutdown_host $IP_POLO wait
+  #python3 fog.py deploy $HOST_IES $HOST_POLO --wait
+  wait_for_host $IP_IES
+  wait_for_host $IP_POLO
+  scp ~/Workspace/AVAPolos/build/avapolos_beta* admin@$IP_IES:~/
+  ssh admin@$IP_IES "sudo ./avapolos_beta* -y"
+}
+
+while true; do
+  case "$1" in
+    --version | -v)
+      #Print version information
+      echo "AVAPolos versão: $INSTALLER_VERSION"
+	    exit 0
+	  ;;
+    --help | -h)
+      usage
+      exit 0
+	  ;;
+    --loglvl)
+      shift
+      echo "alterando nível de log para: $1" | log info
+      export $LOGGER_LVL="$1"
+      echo "$1" > "$ETC_PATH/logger.conf"
+      shift
+    ;;
+    --test)
+      echo "Iniciando testes."
+      test
+      exit 0
+    ;;
+    *)
+      test
+      exit 0
+    ;;
+  esac
+done
