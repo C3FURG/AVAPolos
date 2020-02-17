9e73d5db5df5ba6eaab84f7f5e1a1f12605d431a
Rafael Souza <rsouza19796@gmail.com>
2020-02-17 16:06:48 -0300

    Working on #26 and #28.

--- build/avapolos/data/services/basic/resources/controle/public/header.php
+++ build/avapolos/data/services/basic/resources/controle/public/header.php
@@ -8,33 +8,32 @@
 
   <title>AVAPolos - Controle</title>
 
-  <!-- Xterm.js -->
-  <link href="vendor/xterm.min.css" rel="stylesheet">
-  <script src="vendor/xterm.min.js"></script>
 
   <!-- jQuery -->
-  <script src="vendor/jquery-3.3.1.min.js"></script>
-
-  <!-- Template stuff -->
-  <link href="css/controle.min.css" rel="stylesheet">
-  <!-- <script src="vendor/sb-admin.min.js"></script> -->
-
-  <!-- Font -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <script src="vendor/jquery/jquery.min.js" type="text/javascript"></script>
 
   <!-- Popper -->
-  <script src="vendor/popper.min.js"></script>
-
+  <script src="vendor/popper/popper.min.js" type="text/javascript"></script>
+  
   <!-- BS4 -->
-  <link href="vendor/bootstrap.min.css" rel="stylesheet">
-  <script src="vendor/bootstrap.min.js"></script>
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+
+  <!-- Template stuff -->
+  <link href="css/controle.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Font -->
+  <link href="vendor/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
+
+
 
   <!-- SweetAlert -->
-  <script src='sweetalert2.all.min.js'></script>
+  <script src='vendor/sweetalert/sweetalert2.all.min.js'></script>
 
   <!-- Controle -->
-  <script src='controle.js'></script>
+  <script src='js/controle.js'></script>
 
   <!-- jQuery Mask -->
-  <script src="vendor/jquery.mask.min.js"></script>
+  <script src="vendor/jquery-mask/jquery.mask.min.js"></script>
 </head>
--- build/avapolos/data/services/basic/scripts/controle.sh
+++ build/avapolos/data/services/basic/scripts/controle.sh
@@ -11,32 +11,6 @@ mkdir -p $BASIC_DATA_DIR/controle/public
 
 echo "Copiando recursos do serviço." | log debug data_compiler
 cp -rf $BASIC_RESOURCES_DIR/controle/public $BASIC_DATA_DIR/controle
-mkdir -p $BASIC_DATA_DIR/controle/public/vendor/{fontawesome-free,jquery}
-
-tmp=$(mktemp -d -t controle_vendor_download.XXXXXXX)
-echo "Criando diretório temporário para download de recursos: $tmp" | log debug data_compiler
-echo "Executando o download dos vendors." | log debug data_compiler
-wget -O $tmp/fontawesome-free.zip https://use.fontawesome.com/releases/v5.11.2/fontawesome-free-5.11.2-web.zip | log debug data_compiler
-wget -O $tmp/fileupload.tar.gz "https://github.com/blueimp/jQuery-File-Upload/archive/v10.7.0.tar.gz"
-cd $tmp
-unzip fontawesome-free.zip | log debug data_compiler
-tar xfzv fileupload.tar.gz
-mv jQuery* fileupload
-cd $BASIC_DIR
-cp -rf $tmp/fontawesome-free-5.11.2-web/* $BASIC_DATA_DIR/controle/public/vendor/fontawesome-free/
-cp -rf $tmp/fileupload/js/jquery.fileupload.js $BASIC_DATA_DIR/controle/public/vendor/
-cp -rf $tmp/fileupload/js/jquery.iframe-transport.js $BASIC_DATA_DIR/controle/public/vendor/
-cp -rf $tmp/fileupload/js/vendor/jquery.ui.widget.js $BASIC_DATA_DIR/controle/public/vendor/
-wget -O $BASIC_DATA_DIR/controle/public/vendor/jquery.mask.min.js "https://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js"
-wget -O $BASIC_DATA_DIR/controle/public/vendor/popper.min.js "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-wget -O $BASIC_DATA_DIR/controle/public/vendor/jquery-3.3.1.min.js "https://code.jquery.com/jquery-3.3.1.min.js"
-wget -O $BASIC_DATA_DIR/controle/public/vendor/bootstrap.min.css "https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-wget -O $BASIC_DATA_DIR/controle/public/vendor/bootstrap.min.js "https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-wget -O $BASIC_DATA_DIR/controle/public/vendor/xterm.min.js "https://cdnjs.cloudflare.com/ajax/libs/xterm/3.14.5/xterm.min.js"
-wget -O $BASIC_DATA_DIR/controle/public/vendor/xterm.min.css "https://cdnjs.cloudflare.com/ajax/libs/xterm/3.14.5/xterm.min.css"
-
-echo "vendors instalados com sucesso no serviço." | log debug data_compiler
-
 
 echo "Iniciando webserver" | log debug data_compiler
 docker-compose up -d controle
