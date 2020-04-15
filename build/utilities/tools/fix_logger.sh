6fb62000801ad4fa523731be7c082bafc4b16fb9
Rafael Souza <rsouza19796@gmail.com>
2020-04-15 18:57:03 -0300

    New logger fixing.

--- build/utilities/tools/fix_logger.sh
+++ build/utilities/tools/fix_logger.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
-fix() {
-  sed -i -E "s/\| log debug$/\| log debug "$1"/g" $2;
-  sed -i -E "s/\| log info$/\| log info "$1"/g" $2;
-  sed -i -E "s/\| log warn$/\| log warn "$1"/g" $2;
-  sed -i -E "s/\| log error$/\| log error "$1"/g" $2;
-}
+files=$(find ../../../../ -name 'install_networking.sh')
 
-for file in $(find build/avapolos/scripts/install -name "*.sh"); do
-  fix "installer" $file
+for file in $files; do
+  echo $file
+  grep -En '((^\ +echo)|(^echo)).+\|\ log' "$file" | while read -r line ; do
+
+    number="$(echo $line | cut -d: -f1)"
+    content="$(echo $line | cut -d: -f2)"
+    message="$(echo $content | sed 's/echo//' | rev | cut -d\| -f2 | rev)"
+    logCmd="$(echo $content | rev | cut -d\| -f1 | rev)"
+    logLevel=$(echo $logCmd | awk '{print $2}')
+    newLine="log $logLevel $message"
+
+    echo "Processing line #$number: $content"
+    if [[ "$logCmd" =~ log ]]; then
+      echo "  Message: $message"
+      echo "  Logger command: $logCmd"
+      echo "  Logger level: $logLevel"
+      echo "  New Line: $newLine"
+      sed -i "$number"'s/.*/'"$newLine"'/' $file
+    else
+      echo "  Ignoring this line"
+    fi
+  done
 done
